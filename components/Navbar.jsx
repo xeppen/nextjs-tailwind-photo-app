886aae091f3ee4004a71acbebd00711ee6949261
Sebastian Ljungberg <xeppen@gmail.com>
2022-12-28 21:25:29 +0100

    Initial setup

---
+++ components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <Link href="/">
+          <h1>Captur</h1>
+        </Link>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/#gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Work</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
